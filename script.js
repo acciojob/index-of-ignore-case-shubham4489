733742e567770e775f7fa9409ec801235722bae6
shubham4489 <143184073+shubham4489@users.noreply.github.com>
2024-08-29 16:34:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	const lowerStr =str.toLowerCase();
+	const subLowerStr= str.toLowerCase();
+
+	return lowerStr.indexOf(subLowerStr);
+	
 }
 
-// Please do not change the code below
+// // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
